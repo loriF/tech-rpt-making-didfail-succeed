5e48739b0a9d470240624caaad513eb277a576ea
wklieber <weklieber@cert.org>
2015-01-09 13:21:06 -0500

    From ShareLatex

--- content/java/broadcastdynrcvr.java
+++ content/java/broadcastdynrcvr.java
@@ -1,10 +1,8 @@
-public class MyBroadcastReceiver_Dyn 
-        extends BroadcastReceiver { 
-    public void onReceive(
-            Context c, 
-            Intent i) { 
+public class MyBroadcastReceiver_Dyn extends BroadcastReceiver { 
+    public void onReceive(Context c, Intent i) { 
         String file = "devicelDSinkFile.txt"; 
         Bundle extras = i.getExtras(); 
         String id = extras.get("deviceID");
         // Write id to the file...
+    }
 }
--- content/java/broadcastonreceive.java
+++ content/java/broadcastonreceive.java
@@ -1,10 +1,7 @@
-public void onReceive(
-        Context context, 
-        Intent intent) { 
+public void onReceive(Context context, Intent intent) { 
     Bundle extras = intent.getExtras(); 
     SmsManager smsManager = ...;
     String number = "55555555555"; 
     String id = extras.get("deviceID");
-    smsManager.sendTextMessage(
-        number, null, id, null, null); 
+    smsManager.sendTextMessage(number, null, id, null, null); 
 } 
