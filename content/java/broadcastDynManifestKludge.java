bdf9f9384313c9665c4de79266a4a04416e1cfc0
lflynn <lflynn@cert.org>
2015-01-23 22:00:01 -0500

    text for kludge filter code in manifest

---
+++ content/java/broadcastDynManifestKludge.java
@@ -0,0 +1,5 @@
+<receiver android:name="MyBroadcastReceiver_Dyn" android:exported="true">
+     <intent-filter>
+          <action android:name="DeviceIDBroadcast" />
+     </intent-filter>
+</receiver>
